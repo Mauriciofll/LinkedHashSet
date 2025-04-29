d8b796abc4c5010773e2a8a9587ac3e1b56bc608
Mauricio Fermino Lopes <mauricio.lopes@cs.up.edu.br>
2025-04-29 16:59:40 -0300

    Exemplo de LinkedHashSet Signed-off-by: Mauricio Fermino Lopes <mauricio.lopes@cs.up.edu.br>

---
+++ ExemploLinkedHashSet.java
@@ -0,0 +1,23 @@
+import java.util.LinkedHashSet;
+
+public class ExemploLinkedHashSet {
+    public static void main(String[] args) {
+        // Criando um LinkedHashSet de Strings
+        LinkedHashSet<String> frutas = new LinkedHashSet<>();
+
+        // Adicionando elementos
+        frutas.add("Maçã");
+        frutas.add("Banana");
+        frutas.add("Laranja");
+        frutas.add("Banana"); // Está repetida, portanto não será adicionada de novo
+        frutas.add("Uva");
+        frutas.add("Kiwi");
+        frutas.add("Pera");
+        frutas.add("Kiwi"); // Está repetida novamente
+
+
+        // Resultado do LinkedHashSet
+        // A ordem de inserção é mantida e elementos duplicados não são adicionados
+        System.out.println("Frutas: " + frutas);
+    }
+}
